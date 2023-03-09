0617485adbbe85b6fbb860db9e8a854a1bf290e8
Guilherme Caeiro de Mattos <gcm_nacional@ymail.com>
2023-03-09 07:45:31 -0300

    Forgot to commit a file. Big oopsie!

---
+++ adaptation.jl
@@ -0,0 +1,20 @@
+
+function reset_commoner_population(environment::Environment, elite::Vector{Individual})
+    println("Resetting commoners.")
+    commoners = initialize_population(environment) # could temporarily change population size to avoid generating more individuals than needed
+    needed_commoners = environment.population_size - length(elite)
+    print("needed_commoners ", needed_commoners)
+    return [elite; commoners[1:needed_commoners]]
+end
+
+function adapt(environment::Environment, elite::Vector{Individual})
+    population = Vector{Individual}()
+
+    if environment.adaptation_method == "reset"
+        population = reset_commoner_population(environment, elite)
+    else
+        throw(error("Unknow adaptation method ", environment.adaptation_method))
+    end
+
+    return population
+end

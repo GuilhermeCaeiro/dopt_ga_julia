79e0b6298e0d99f180472ff3f2da00eba6152821
thj3a <arthur.ea@hotmail.com>
2023-03-22 10:10:58 -0300

    fixing mutation definition names

--- mutation.jl
+++ mutation.jl
@@ -1,7 +1,7 @@
 using StatsBase
 using Distributions
 
-function binary_singlepoint_mutation(chromosome::Vector{Int64}, environment::Environment)
+function binary_singlepoint_mutation!(chromosome::Vector{Int64}, environment::Environment)
     ones = findall(x -> x == 1, chromosome)
     zeros = findall(x -> x == 0, chromosome)
 
@@ -16,7 +16,7 @@ function binary_singlepoint_mutation(chromosome::Vector{Int64}, environment::Env
     return chromosome, zero_index, one_index 
 end
 
-function binary_percentchange_mutation(chromosome::Vector{Int64}, environment::Environment, percent)#total_ones::Int64, percent::Float64)
+function binary_percentchange_mutation!(chromosome::Vector{Int64}, environment::Environment, percent)#total_ones::Int64, percent::Float64)
     ones = findall(x -> x == 1, chromosome)
     zeros = findall(x -> x == 0, chromosome)
 
@@ -32,7 +32,7 @@ function binary_percentchange_mutation(chromosome::Vector{Int64}, environment::E
     return chromosome, zeros_to_change, ones_to_change
 end
 
-function binary_variablepercentchange_mutation(chromosome::Vector{Int64}, environment::Environment, min_percent, max_percent) #total_ones::Int64, min_percent::Float64, max_percent::Float64)
+function binary_variablepercentchange_mutation!(chromosome::Vector{Int64}, environment::Environment, min_percent, max_percent) #total_ones::Int64, min_percent::Float64, max_percent::Float64)
     percent = rand(Uniform(min_percent, max_percent))
     return binary_percentchange_mutation(chromosome, environment, percent)
 end
@@ -52,7 +52,7 @@ function mutate(environment::Environment, individual::Individual)
     new_zeros = []
     
     if environment.mutation_method == "binary_singlepoint"
-        new_chromosome, new_ones, new_zeros = binary_singlepoint_mutation(new_chromosome, environment)
+        new_chromosome, new_ones, new_zeros = binary_singlepoint_mutation!(new_chromosome, environment)
     elseif environment.mutation_method == "binary_percentchange"
         # mutation_params is assumed to be a float between 0 and 1.
         new_chromosome, new_ones, new_zeros = binary_percentchange_mutation!(new_chromosome, environment, environment.mutation_params[1])
